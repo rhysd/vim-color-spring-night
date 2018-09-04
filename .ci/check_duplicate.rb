960273ee9f7b8bdf332c7ea210ad8ac8a98f17af
rhysd <lin90162@yahoo.co.jp>
2018-09-04 19:44:44 +0900

    fix script to check duplicates

--- .ci/check_duplicate.rb
+++ .ci/check_duplicate.rb
@@ -3,8 +3,8 @@
 # XXX: Ruby on Travis CI is v1.9. __dir__ is unavailable.
 File.read(File.join(File.dirname(File.dirname(__FILE__)), 'colors', 'spring-night.vim'))
   .split("\n")
-  .select{|s| s.start_with? "call s:hi('" }
-  .map{|s| s =~ /^call s:hi\('([^']*)',/; $1}
+  .select{|s| s.start_with? "        \\ ['" }
+  .map{|s| s =~ /^        \\ \['([^']*)',/; $1}
   .group_by{|s| s}
   .each{|n, g| abort "'#{n}' duplicates!" if g.length > 1 }
 
