5c6e4159d900d255460496a93ecb794671d67436
rhysd <lin90162@yahoo.co.jp>
2018-09-05 18:06:05 +0900

    fix check script and travis config

--- .ci/check_duplicate.rb
+++ .ci/check_duplicate.rb
@@ -1,10 +1,9 @@
 #!/usr/bin/env ruby
 
 # XXX: Ruby on Travis CI is v1.9. __dir__ is unavailable.
-File.read(File.join(File.dirname(File.dirname(__FILE__)), 'colors', 'spring-night.vim'))
-  .split("\n")
-  .select{|s| s.start_with? "        \\ ['" }
-  .map{|s| s =~ /^        \\ \['([^']*)',/; $1}
+File.foreach(File.join(File.dirname(File.dirname(__FILE__)), 'colors', 'spring-night.vim'))
+  .select{|s| s.start_with?("hi ") || s.start_with?("exe 'hi' ") }
+  .map{|s| s =~ (s.start_with?("hi ") ?  /^hi (\w+)/ : /^exe 'hi' '(\w+)'/); $1 }
   .group_by{|s| s}
   .each{|n, g| abort "'#{n}' duplicates!" if g.length > 1 }
 
